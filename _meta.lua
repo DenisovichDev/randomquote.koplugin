9c9dea64113f52daa7e5d0840e1827c329b84858
DenisovichDev <bhaswarishi2908@gmail.com>
2025-12-17 03:30:17 +0530

    License information

--- _meta.lua
+++ _meta.lua
@@ -1,3 +1,5 @@
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
 return {
     name = "randomquote",
     fullname = "Random Quote on Lock",
--- quotes.lua
+++ quotes.lua
@@ -1,3 +1,5 @@
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
 -- Placeholder quotes for randomquote plugin
 -- The module returns an array (table) of quote objects: { text=..., book=..., author=... }
 
